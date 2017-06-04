08e767ab0ca2b6fb2e03e79f2d004bdad526dc6f
Atsushi Yoshinari <a.bit.of.elbowroomer@gmail.com>
2017-06-05 00:14:17 +0900

    el-get の設定を追加した

---
+++ .emacs.d/README.md
@@ -0,0 +1,7 @@
+## 下準備
+
+### el-get のインストール
+
+```
+$ git clone https://github.com/dimitri/el-get.git
+```
--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -3,6 +3,18 @@
 ;;  ---------------
 
 ;; ロードパス
+(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
+
+;; el-get
+(unless (require 'el-get nil 'noerror)
+  (with-current-buffer
+      (url-retrieve-synchronously
+       "https://raw.githubusercontent.com/dimitri/el-get/master/el-get-install.el")
+    (goto-char (point-max))
+    (eval-print-last-sexp)))
+
+(add-to-list 'el-get-recipe-path "~/.emacs.d/el-get-user/recipes")
+(el-get 'sync)
 
 ;; homebrwe でインストールしたツールを使う
 (add-to-list 'exec-path (expand-file-name "/usr/local/bin"))
