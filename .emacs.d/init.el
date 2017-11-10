1c532d656d5c374f4ddcc2bc56afc9dd2724ab58
Atsushi Yoshinari <a.bit.of.elbowroomer@gmail.com>
2017-11-10 16:54:46 +0900

    js-basic-offest を 2 に

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -382,6 +382,7 @@
 (require 'js2-mode)
 (autoload 'js2-mode "js2-mode" nil t)
 (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+(setq-default js2-basic-offset 2)
 
 ;; haml-mode
 (require 'haml-mode)
