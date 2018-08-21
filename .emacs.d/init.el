0c0d1329a001358f3379953e335e80110dcd27a6
Atsushi Yoshinari <a.bit.of.elbowroomer@gmail.com>
2018-08-21 11:09:02 +0900

    emacs で jsx に対応

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -16,6 +16,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(flycheck-disabled-checkers (quote (javascript-jshint javascript-jscs)))
  '(package-selected-packages
    (quote
     (rufo web-mode ac-emmet emmet-mode emmet haml-mode js2-mode ag projectile-rails projectile rubocop ruby-block ruby-electric enh-ruby-mode anything flycheck auto-complete inflections)))
@@ -401,6 +402,8 @@ With argument, do this that many times."
 (require 'js2-mode)
 (autoload 'js2-mode "js2-mode" nil t)
 (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+(add-to-list 'auto-mode-alist '("\\.jsx\\'" . js2-jsx-mode))
+(add-hook 'js2-jsx-mode-hook 'emmet-mode)
 (setq-default js2-basic-offset 2)
 
 ;; haml-mode
@@ -428,6 +431,7 @@ With argument, do this that many times."
 ;; emmet-mode
 (require 'emmet-mode)
 (add-hook 'sgml-mode-hook 'emmet-mode) ;; マークアップ言語全部で使う
+(setq emmet-expand-jsx-className? t) ;; jsx で使う
 (eval-after-load "emmet-mode"
   '(define-key emmet-mode-keymap (kbd "C-j") nil)) ;; C-j は newline のままにしておく
 (keyboard-translate ?\C-i ?\H-i) ;;C-i と Tabの被りを回避
