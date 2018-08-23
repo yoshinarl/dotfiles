7fe4c02aa4ca9fff159d7edde58c8df013e3b569
yoshinari <a.bit.of.elbowroomer@gmail.com>
2018-08-23 18:03:18 +0900

    .pryrc を追加

--- linkDotfiles.sh
+++ linkDotfiles.sh
@@ -4,3 +4,4 @@ ln -sf ~/.dotfiles/.emacs.d ~/.emacs.d
 ln -sf ~/.dotfiles/.zshrc ~/.zshrc
 ln -sf ~/.dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/.dotfiles/.inputrc ~/.inputrc
+ln -sf ~/.dotfiles/.pryrc ~/.pryrc
