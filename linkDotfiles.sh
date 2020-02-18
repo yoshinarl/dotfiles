685fc031cf575e4e67af9506f3b69f99155430f7
yoshinari <a.bit.of.elbowroomer@gmail.com>
2020-02-18 17:37:50 +0900

    add .tigrc

--- linkDotfiles.sh
+++ linkDotfiles.sh
@@ -6,3 +6,4 @@ ln -sf ~/.dotfiles/.zshrc ~/.zshrc
 ln -sf ~/.dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/.dotfiles/.inputrc ~/.inputrc
 ln -sf ~/.dotfiles/.pryrc ~/.pryrc
+ln -sf ~/.dotfiles/.tigrc ~/.tigrc
