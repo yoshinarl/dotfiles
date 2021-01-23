d998589ace49df103faa21dff248738388a1aa4e
Atsushi Yoshinari <7158259+yoshinarl@users.noreply.github.com>
2021-01-23 17:58:35 +0900

    linkDotfiles.sh を修正

--- linkDotfiles.sh
+++ linkDotfiles.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-ln -sf ~/.dotfiles/.emacs.d ~/.emacs.d
-ln -sf ~/.dotfiles/.zshrc ~/.zshrc
-ln -sf ~/.dotfiles/.gitconfig ~/.gitconfig
-ln -sf ~/.dotfiles/.inputrc ~/.inputrc
-ln -sf ~/.dotfiles/.pryrc ~/.pryrc
-ln -sf ~/.dotfiles/.Brewfile ~/.Brewfile
+ln -is .emacs.d ~/.emacs.d
+ln -is .zshrc ~/.zshrc
+ln -is .gitconfig ~/.gitconfig
+ln -is .inputrc ~/.inputrc
+ln -is .pryrc ~/.pryrc
+ln -is .Brewfile ~/.Brewfile
