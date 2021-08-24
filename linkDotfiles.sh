46c315016368965e3e98c76add4d680a71c7e336
Atsushi Yoshinari <7158259+yoshinarl@users.noreply.github.com>
2021-08-24 11:38:49 +0900

    .gitignore への ln を追加

--- linkDotfiles.sh
+++ linkDotfiles.sh
@@ -4,6 +4,7 @@ PWD=$(cd $(dirname $0);pwd)
 ln -is $PWD/.emacs.d ~/.emacs.d
 ln -is $PWD/.zshrc ~/.zshrc
 ln -is $PWD/.gitconfig ~/.gitconfig
+ln -is $PWD/.gitignore ~/.gitignore
 ln -is $PWD/.inputrc ~/.inputrc
 ln -is $PWD/.pryrc ~/.pryrc
 ln -is $PWD/.Brewfile ~/.Brewfile
