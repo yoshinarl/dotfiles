f01bd9ac2624ad706102fe9cc94acad3e8d8f6b2
mustan <7158259+yoshinarl@users.noreply.github.com>
2023-06-25 23:59:20 +0900

    Package 関係のコードを削除

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -8,29 +8,6 @@
 ;; ロードパス
 (setq load-path (cons "~/.emacs.d/elisp" load-path))
 
-;; Added by Package.el.  This must come before configurations of
-;; installed packages.  Don't delete this line.  If you don't want it,
-;; just comment it out by adding a semicolon to the start of the line.
-;; You may delete these explanatory comments.
-(package-initialize)
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(flycheck-disabled-checkers (quote (javascript-jshint javascript-jscs)))
- '(package-selected-packages
-   (quote
-    (exec-path-from-shell tide typescript-mode rubocop rufo web-mode ac-emmet emmet-mode emmet haml-mode js2-mode ag ruby-block ruby-electric enh-ruby-mode flycheck auto-complete inflections)))
- '(session-use-package t nil (session)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
-
 ;; 環境変数パス
 (dolist (dir (list
               "/sbin"
