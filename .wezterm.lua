d6a50877b8eef88f46df375645d9e2ccbc32101c
mustan <7158259+yoshinarl@users.noreply.github.com>
2025-05-31 19:25:43 +0900

    Add wezterm Japanese font settings

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,4 +1,12 @@
 local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
+config.use_ime = true
+config.font = wezterm.font "HackGen Console NF"
+-- ↑の設定で日本語入力が動かないことがあったので以下も追加しておく
+wezterm.font_with_fallback({
+  "Noto Sans Mono CJK JP",
+  "HackGen Console NF"
+})
+
 return config
