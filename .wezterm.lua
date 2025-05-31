fc56c18d747abb268af6190d5e022225ec7a9890
mustan <7158259+yoshinarl@users.noreply.github.com>
2025-05-31 19:35:21 +0900

    Add OS-specific configuration handling for wezterm

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,6 +1,16 @@
 local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
+local os_name = wezterm.target_triple:match("darwin") and "macos"
+             or wezterm.target_triple:match("linux") and "linux"
+             or wezterm.target_triple:match("windows") and "windows"
+
+if os_name == "macos" then
+elseif os_name == "linux" then
+  config.enable_wayland = false
+elseif os_name == "windows" then
+end
+
 config.use_ime = true
 config.font = wezterm.font "HackGen Console NF"
 -- ↑の設定で日本語入力が動かないことがあったので以下も追加しておく
